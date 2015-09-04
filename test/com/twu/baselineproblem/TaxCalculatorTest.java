ffd386bba0182b264a8c904fb734b935dd5cf421
maharjun <maharjun@thoughtworks.com>
2015-09-04 16:52:31 +0530

    Added spec for calculating tax value for imported elements

--- test/com/twu/baselineproblem/TaxCalculatorTest.java
+++ test/com/twu/baselineproblem/TaxCalculatorTest.java
@@ -11,4 +11,11 @@ public class TaxCalculatorTest {
 
         Assert.assertEquals(0.5, taxCalculator.calculateTax(), 0.2d);
     }
+
+    @Test
+    public void shouldCalculateTaxForNonImportedItems() {
+        TaxCalculator taxCalculator = new TaxCalculator(10, 10.0);
+
+        Assert.assertEquals(1.0, taxCalculator.calculateTax(), 0.2d);
+    }
 }
